9c98e1f9ae64f36a83c7f8f8eee85b0a94bf13d9
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-08-15 10:41:05 +0900

    should fail

--- test/unit/touch_action_tests.py
+++ test/unit/touch_action_tests.py
@@ -29,9 +29,7 @@ class TouchActionTests(unittest.TestCase):
         self.assertEqual(json, self._touch_action.json_wire_gestures)
 
     def test_tap_x_y_json(self):
-        json = [
-            {'action': 'tap', 'options': {'x': 3, 'y': 4, 'count': 1, 'element': 2}}
-        ]
+        json = [{'action': 'tap', 'options': {'x': 3, 'y': 4, 'count': 1, 'element': 2}}]
         self._touch_action.tap(ElementStub(2), 3, 4)
         self.assertEqual(json, self._touch_action.json_wire_gestures)
 
